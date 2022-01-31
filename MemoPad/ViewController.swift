f4ebcda0e3fd2e621f9b368850e98629fb1714c5
Emily Nozaki <emiri3128.gmail.com>
2022-01-31 23:39:49 +0900

    Initial Commit

---
+++ MemoPad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoPad
+//
+//  Created by Emily Nozaki on 2022/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
