53f5f44fc4ca9a73073180e6891697fab8e490f5
Emily Nozaki <emiri3128.gmail.com>
2022-02-01 01:26:11 +0900

    done

--- MemoPad/TopViewController.swift
+++ MemoPad/TopViewController.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 class TopViewController: UIViewController {
+    
+    
+    @IBOutlet weak var button: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        button.layer.cornerRadius = 25
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- MemoPad/ViewController.swift
+++ MemoPad/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var saveData: UserDefaults = UserDefaults.standard
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet var contentTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        titleTextField.delegate = self
+        
+        titleTextField.text = saveData.object(forKey: "title") as? String
+        contentTextView.text = saveData.object(forKey: "content") as? String
+        
     }
-
-
+    @IBAction func saveMemo() {
+        
+        saveData.set(titleTextField.text, forKey: "title")
+        saveData.set(contentTextView.text, forKey: "content")
+        
+        let alert: UIAlertController = UIAlertController(title: "保存", message: "メモの保存が完了しました", preferredStyle: .alert)
+        
+        alert.addAction(
+            UIAlertAction(
+                title: "OK",
+                style: .default,
+                handler: { _ in
+                    self.navigationController?.popViewController(animated: true)
+                    print("OKボタンが押されました")
+                }
+            )
+        )
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //自動でキーボード下げる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
 }
 
